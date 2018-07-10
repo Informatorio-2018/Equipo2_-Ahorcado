e068260270212569738b0beca81942f55cfc4d45
joel <joemaglioni1@gmail.com>
2018-07-10 00:52:12 -0300

    subi carpeta modulo

---
+++ modulos/modulo_pickle.py
@@ -0,0 +1,21 @@
+from pickle import *
+
+def pasa_dic():
+	palabras={}
+	palabras["ARBOL"]=["Pista de Arbol"]
+	palabras["CASA"]=["Pista de casa"]
+	palabras["SILLA"]=["Pista de silla"]
+	palabras["AUDIO"]=["Pista de audio"]
+	palabras["VENTANA"]=["Pista de ventana"]
+	palabras["COMPUTADORA"]=["Pista de computadora"]
+	palabras["RELOJ"]=["Pista de reloj"]
+	palabras["AVION"]=["Pista de avion"]
+	palabras["MILANESA"]=["Pista de milanesa"]
+	
+	pickle_file = open('palabras.pickle', 'wb')
+	dump(palabras, pickle_file)
+
+	pickle_file = open('palabras.pickle','rb')
+	contenido_dic = load(pickle_file)
+
+	return contenido_dic
