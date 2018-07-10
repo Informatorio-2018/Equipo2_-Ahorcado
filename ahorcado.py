371fb1412b8e01888e2e8d79a267cf8475665b84
joel <joemaglioni1@gmail.com>
2018-07-10 02:53:24 -0300

    todo a la normalidad

--- ahorcado.py
+++ ahorcado.py
@@ -1,31 +1,9 @@
-from modulo_pickle import *
-import random
 from pickle import * 
 
 
 
 class Ahorcado():
-	
-	def diccionario(self):
-		recibir_dic=pasa_dic()
-		return recibir_dic
-
-	def lista_random(self):
-		recibir_dic=pasa_dic()
-		lista=recibir_dic.keys()
-		pal_elegida = random.choice(list(recibir_dic.items()))
-		print(pal_elegida[0])
-		print(pal_elegida[1])
+	pass
 
 
 
-
-Ahorcado1=Ahorcado()
-
-print(Ahorcado1.diccionario())
-input('')
-print()
-
-Ahorcado1.lista_random()
-
-input('')
