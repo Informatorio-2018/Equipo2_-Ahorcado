865b53f66d3a12b22e654c800403a3d24ec626e6
joel <joemaglioni1@gmail.com>
2018-07-10 02:47:49 -0300

    prueba

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,31 @@
+from modulo_pickle import *
+import random
 from pickle import * 
 
 
+
 class Ahorcado():
-	pass
+	
+	def diccionario(self):
+		recibir_dic=pasa_dic()
+		return recibir_dic
+
+	def lista_random(self):
+		recibir_dic=pasa_dic()
+		lista=recibir_dic.keys()
+		pal_elegida = random.choice(list(recibir_dic.items()))
+		print(pal_elegida[0])
+		print(pal_elegida[1])
+
+
+
+
+Ahorcado1=Ahorcado()
+
+print(Ahorcado1.diccionario())
+input('')
+print()
+
+Ahorcado1.lista_random()
+
+input('')
