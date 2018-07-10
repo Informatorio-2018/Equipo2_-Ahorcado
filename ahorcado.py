770b9c9d105e5d65eb0ea9d804b676020692d489
joel <joemaglioni1@gmail.com>
2018-07-10 19:57:24 -0300

    tarea 1 y 2

--- ahorcado.py
+++ ahorcado.py
@@ -1,18 +1,32 @@
 from pickle import * 
-
-
-
-
-
-
-
+from modulo_pickle import *
+import random
 
 
 class Ahorcado():
-	pass
+
+	def lista_palabra(self):
+		diccionario=pasa_dic()
+		lista=list(diccionario.keys())
+		return lista
+
+	def buscar_palabra_aleatoria(self):
+		lista_de_palabras=self.lista_palabra()
+		palabra_aleatoria=random.choice(lista_de_palabras)
+		return palabra_aleatoria
 
 
 
+
+ahorcado1=Ahorcado()
+
+print(ahorcado1.lista_palabra())
+print()
+print()
+
+print(ahorcado1.buscar_palabra_aleatoria())
+
+input('')
 
 
 
--- modulo_pickle.py
+++ modulo_pickle.py
@@ -2,15 +2,15 @@ from pickle import *
 
 def pasa_dic():
 	palabras={}
-	palabras["ARBOL"]=["Pista de Arbol"]
-	palabras["CASA"]=["Pista de casa"]
-	palabras["SILLA"]=["Pista de silla"]
-	palabras["AUDIO"]=["Pista de audio"]
-	palabras["VENTANA"]=["Pista de ventana"]
-	palabras["COMPUTADORA"]=["Pista de computadora"]
-	palabras["RELOJ"]=["Pista de reloj"]
-	palabras["AVION"]=["Pista de avion"]
-	palabras["MILANESA"]=["Pista de milanesa"]
+	palabras["ARBOL"]="Pista de Arbol"
+	palabras["CASA"]="Pista de casa"
+	palabras["SILLA"]="Pista de silla"
+	palabras["AUDIO"]="Pista de audio"
+	palabras["VENTANA"]="Pista de ventana"
+	palabras["COMPUTADORA"]="Pista de computadora"
+	palabras["RELOJ"]="Pista de reloj"
+	palabras["AVION"]="Pista de avion"
+	palabras["MILANESA"]="Pista de milanesa"
 	
 	pickle_file = open('palabras.pickle', 'wb')
 	dump(palabras, pickle_file)
